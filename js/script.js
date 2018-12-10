1109a064a96b87391fd439426b7686eb1ed7673b
Ruben Ponce <ruben_ponce1@yahoo.com>
2018-12-10 04:51:11 -0800

    added a search bar functionality. next will add modal prev and next buttons

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //XML requests
 let xhr = new XMLHttpRequest();
-const url = 'https://randomuser.me/api/?format=json&nat=US';
+const url = 'https://randomuser.me/api/?format=json&nat=US&results=12';
 
 //let declarations
 let empCardMarkup = [];
@@ -8,7 +8,13 @@ let empModel = [];
 let gallery = document.querySelector('#gallery');
 let body = document.querySelector('body');
 let employeeIndex = 0;
+
 let maxEmployees= 12;
+let searchBar = document.querySelector('div[class="search-container"]');
+createSearchBar();
+let searchInput = document.querySelector('#search-input');
+
+const nameRegex = new RegExp(/^[A-Za-z]{0,30}$/);
 
 //----------------------function declarations-------------------------- 
 
@@ -21,6 +27,15 @@ div.remove(div);
 appendClickEvent();
 }
 
+function checkNameMatch(name,value) {
+    if(name.includes(value)){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
 
 
 //add the click events to the cards.
@@ -45,7 +60,13 @@ function appendEmpCards(dataPart){
     gallery.innerHTML += dataPart;
    // dataPiece.addEventListener("click", => displayEmpModel(dataPiece))
 }
-
+function createSearchBar(){
+    searchBar.innerHTML += `
+    <form action="#" method="get">
+    <input type="search" id="search-input" class="search-input" placeholder="Search...">
+    <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+</form>`
+}
 
 function createEmpModel(data){
     return `
@@ -53,26 +74,26 @@ function createEmpModel(data){
     <div class="modal">
         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
         <div class="modal-info-container">
-            <img class="modal-img" src=${data.results[0].picture.large} alt="profile picture">
-            <h3 id="name" class="modal-name cap">${data.results[0].name.first} ${data.results[0].name.last}</h3>
-            <p class="modal-text">${data.results[0].email}</p>
-            <p class="modal-text cap">${data.results[0].location.city}</p>
+            <img class="modal-img" src=${data.picture.large} alt="profile picture">
+            <h3 id="name" class="modal-name cap">${data.name.first} ${data.name.last}</h3>
+            <p class="modal-text">${data.email}</p>
+            <p class="modal-text cap">${data.location.city}</p>
             <hr>
-            <p class="modal-text">${data.results[0].cell}</p>
-            <p class="modal-text">${(data.results[0].location.street).toUpperCase()}, ${(data.results[0].location.city).toUpperCase()}, ${(data.results[0].location.state).toUpperCase()} ${data.results[0].location.postcode}</p>
-            <p class="modal-text">Birthday: ${(data.results[0].dob.date).slice(0,10)}</p>
+            <p class="modal-text">${data.cell}</p>
+            <p class="modal-text">${(data.location.street).toUpperCase()}, ${(data.location.city).toUpperCase()}, ${(data.location.state).toUpperCase()} ${data.location.postcode}</p>
+            <p class="modal-text">Birthday: ${(data.dob.date).slice(0,10)}</p>
         </div>
     </div>
     `
 }
 
 function createEmpData(data){
-    return ` <div class="card"> <div class="card-img-container">   <img class="card-img" src=${data.results[0].picture.large} alt="profile picture">
+    return ` <div class="card"> <div class="card-img-container">   <img class="card-img" src=${data.picture.large} alt="profile picture">
                         </div>
                         <div class="card-info-container">
-                            <h3 id="name" class="card-name cap">${data.results[0].name.first} ${data.results[0].name.last}</h3>
-                            <p class="card-text">${data.results[0].email}</p>
-                            <p class="card-text cap">${data.results[0].location.city}, ${data.results[0].location.state}</p>
+                            <h3 id="name" class="card-name cap">${data.name.first} ${data.name.last}</h3>
+                            <p class="card-text">${data.email}</p>
+                            <p class="card-text cap">${data.location.city}, ${data.location.state}</p>
                         </div>
                     </div>
     `
@@ -80,28 +101,56 @@ function createEmpData(data){
 //----------------------end function declarations-------------------------- 
 
 //----------------------main application ----------------------
-while(employeeIndex<12){
+
     //gathers employee data 12 times
     fetch(url)
         .then(response => {    
             return response.text();
     })
         .then(data => {
-            employeeData = JSON.parse(data);//push all the JSON data into the 'empCardMarkup' array.
-           empCardMarkup.push(createEmpData(employeeData));
-            empModel.push(createEmpModel(employeeData));
+            //push all the JSON data into the 'empCardMarkup' array.
+           console.log(JSON.parse(data));
+           let employeeData = JSON.parse(data);
+           console.log(employeeData.results);
+            employeeData.results.forEach(result =>{
+                console.log(result);
+               
+                empCardMarkup.push( createEmpData(result) );
+                empModel.push(createEmpModel(result));
+            })//end foreach
 
-            
             if(empCardMarkup.length ===maxEmployees){//if employees finish loading, display them all and replace loading message.
                         empCardMarkup.forEach(dataPiece => {
                             appendEmpCards(dataPiece);
                                 });
                               appendClickEvent();
+                              document.querySelector("#employee-load").style.display = "none";
+                              
+                              let nameCollection = [...document.querySelectorAll('#name')];
+                             searchInput.focus();
+                              searchInput.onkeyup = function(){
+                               const nameText = nameCollection.map(names => names.textContent);
+                               for (let i = 0; i < nameText.length; i++) {
+                                    if( !checkNameMatch(nameText[i],searchInput.value) ) {
+                                        console.log(nameText[i]);
+                                        nameCollection[i].parentElement.parentElement.style.display = "none";
+                                    } else{
+                                        nameCollection[i].parentElement.parentElement.style.display = "flex";
+                                    }
+                                //end foreach NameText
+                                   
+                                   
+                                   
+                               }
+                                console.log(searchInput.value);
+                            }//end keyUp
                 }
-                document.querySelector("#employee-load").style.display = "none";
-            }  
+               //add search bar functionality here
+               
+            }
+
     );
-    employeeIndex++;
-    }
+
+    
 // ----------------------end main application ----------------------
   
