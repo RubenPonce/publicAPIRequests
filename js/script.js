d543eaf41e91a8b61976c00c400136072a2556db
Ruben Ponce <ruben_ponce1@yahoo.com>
2018-12-09 23:28:41 -0800

    added a way to close the modal, but click events do not persist after initial click

--- js/script.js
+++ js/script.js
@@ -10,10 +10,29 @@ let body = document.querySelector('body');
 let employeeIndex = 0;
 let maxEmployees= 12;
 
-//----function declarations 
+//----------------------function declarations-------------------------- 
+
 function appendEmpModel(dataPart){
 body.innerHTML += dataPart
+document.querySelector('#modal-close-btn').onclick = function (){
+document.querySelector('div[class = "modal-container"]').style.display = 'none';
+
 }
+  appendClickEvent();
+}
+//add the click events to the cards.
+function appendClickEvent(){
+    let empCards = [...document.querySelectorAll('.card')];
+    
+            for(let i=0; i<empCards.length; i++) {
+                console.log(i);
+                empCards[i].onclick = function(){
+                
+                appendEmpModel(empModel[i]);
+            };//end click handler
+         }//end forEach empCard
+        }
+
 
 function appendEmpCards(dataPart){
     gallery.innerHTML += dataPart;
@@ -51,7 +70,9 @@ function createEmpData(data){
                     </div>
     `
 }
-//----end function declarations
+//----------------------end function declarations-------------------------- 
+
+//----------------------main application ----------------------
 while(employeeIndex<12){
     //gathers employee data 12 times
     fetch(url)
@@ -71,41 +92,9 @@ while(employeeIndex<12){
                               appendClickEvent();
                 }
                 document.querySelector("#employee-load").style.display = "none";
-                
-            
-        
-            }
-              
+            }  
     );
     employeeIndex++;
     }
-
-    
-    function appendClickEvent(){
-const empCards = [...document.querySelectorAll('.card')];
-console.log(empCards[0]);
-        for(let i=0; i<empCards.length+1; i++) {
-            empCards[i].addEventListener('click', function(){
-            console.log(i);
-            appendEmpModel(empModel[i]);
-        });//end click handler
-     }//end forEach empCard
-    }
-
-   /* 
-   <div class="modal-container">
-    <div class="modal">
-        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-        <div class="modal-info-container">
-            <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-            <h3 id="name" class="modal-name cap">name</h3>
-            <p class="modal-text">email</p>
-            <p class="modal-text cap">city</p>
-            <hr>
-            <p class="modal-text">(555) 555-5555</p>
-            <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-            <p class="modal-text">Birthday: 10/21/2015</p>
-        </div>
-    </div>
-*/
-
+// ----------------------end main application ----------------------
+  
